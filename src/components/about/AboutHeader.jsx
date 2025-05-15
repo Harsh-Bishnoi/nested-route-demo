bf3e5eae824b33bd5b6b303356251779e7e86a4b
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-15 16:38:23 +0530

    add aboutHeader

--- src/components/about/AboutHeader.jsx
+++ src/components/about/AboutHeader.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Hero from '../common/Hero'
 
 const AboutHeader = () => {
   return (
-      <>
-      
-      </>
+    <>
+      <div className="flex justify-center items-center flex-col bg-[url(../../src/assets/images/png/about-bg-img.webp)] bg-no-repeat bg-cover bg-center relative">
+        <div className="opacity-60 bg-black absolute inset-0"></div>
+        <div className="max-w-[1140px] w-full z-1">
+          <Hero />
+        </div>
+        <div className="max-w-[1140px] px-4 w-full py-32 z-0">
+          <h2 className='font-family-primary text-[40px] leading-[normal] sm:text-6xl text-white'> Luxury & Supercar Brand</h2>
+          <p className='font-family-primary text-2xl max-w-[902px] text-white pt-5'>Born from a legacy of excellence, we create more than just cars — we craft moving masterpieces. Every model reflects a relentless pursuit of performance, elegance, and innovation. Designed for visionaries, built without compromise — our supercars are the ultimate expression of power, prestige, and personal style.</p>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/view/AboutPage.jsx
+++ src/view/AboutPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from '../components/common/Footer'
+import AboutHeader from '../components/about/AboutHeader'
 
 const AboutPage = () => {
   return (
     <>
+      <AboutHeader/>
       <Footer/>
       </>
   )
