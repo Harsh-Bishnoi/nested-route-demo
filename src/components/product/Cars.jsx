211516e0bd7752d7ca7754ed9a5e7a2aa5410d05
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-05-16 14:54:54 +0530

    change in car section

--- src/components/product/Cars.jsx
+++ src/components/product/Cars.jsx
@@ -11,12 +11,12 @@ const Cars = () => {
                 <CustomSubHeading SubHeadingText="Our Products" />
                 <div className="flex flex-wrap justify-between gap-y-6">
                     {CARS_DATA.map((item, index) => (
-                        <div key={index} className="w-full sm:w-[48%] lg:w-[32%] flex justify-center" >
+                        <div key={index} className="w-full sm:w-[48%] lg:w-[32%] mx-auto flex justify-center" >
                             <div className="relative group w-full max-w-[360px] rounded-lg overflow-hidden">
                                 <img className="w-full object-cover" src={item.img} alt="car-img"
                                 />
                                 <div className="absolute inset-0 bg-black bg-opacity-80 opacity-0 group-hover:opacity-70 transition-opacity duration-300 flex items-center justify-center">
-                                    <Link className='font-family-primary text-white text-xl leading-[100%] relative after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-0 after:bg-white after:rounded hover:after:w-full after:duration-300 after:ease-linear' to="/aboutCar">About Car</Link>
+                                    <Link className='font-family-primary text-white text-xl leading-[100%] relative after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-0 after:bg-white after:rounded hover:after:w-full after:duration-300 after:ease-linear' to="/aboutCar">Cars Detail</Link>
                                 </div>
                             </div>
                         </div>
